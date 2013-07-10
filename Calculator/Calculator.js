42977541283c2a78d3c214af882af63c52d15be3
bakeds <bakeds@gmail.com>
2013-07-10 16:14:52 +0200

    eerste nog bugy werkende versie van de calculatpr

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,15 +1,47 @@
 /**
- * Created with JetBrains PhpStorm.
  * User: barld
  * Date: 10-7-13
  * Time: 14:15
- * To change this template use File | Settings | File Templates.
  */
+
+
+// functie om in display getallen en symbolen toe te voegen.
 var addNumber = function(number, form)
 {
     if(form.display.value.length<20)
     {
         var firstnumber = form.display.value;
         form.display.value = firstnumber+number;
+    }
+};
+
+var calculate = function(form)
+{
+    var display = form.display.value;
+    //string moet gespit worden in meerdere delen om er iets mee te kunnen
+    if(display.length >= 3)
+    {
+        var antwoord;
+        if(display.search("\\+") !=-1)//check voor optelsom
+        {
+            var parts = display.split("+");
+            antwoord = parseFloat(parts[0])+parseFloat(parts[1]);
+        }
+        if(display.search("\\-") !=-1)//check voor aftrekken
+        {
+            var parts = display.split("-");
+            antwoord = parseFloat(parts[0])-parseFloat(parts[1]);
+        }
+        if(display.search("\\*") !=-1)//check voor vermenigvuldigen
+        {
+            var parts = display.split("*");
+            antwoord = parseFloat(parts[0])*parseFloat(parts[1]);
+        }
+        form.display.value = antwoord;
+
+    }
+    else
+    {
+        alert("je moet het wel goed invullen");
     }
 };
