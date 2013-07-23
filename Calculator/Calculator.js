d158ad2b6c1de7b5d8fa424b00bda2801b870d5e
bakeds <bakeds@gmail.com>
2013-07-23 16:47:36 +0200

    functie hernoemd

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -8,7 +8,7 @@
 function Calculator()
 {
     // functie om in display getallen en symbolen toe te voegen.
-    this.addNumber = function(number)
+    this.addCharter = function(number)
     {
         if(document.getElementById('display').value.length<20)
         {
@@ -23,22 +23,7 @@ function Calculator()
         //string moet gespit worden in meerdere delen om er iets mee te kunnen
         if(display != parseFloat(display) )
         {
-            var antwoord;
-            if(display.search("\\+") !=-1)//check voor optelsom
-            {
-                var parts = display.split("+");
-                antwoord = parseFloat(parts[0])+parseFloat(parts[1]);
-            }
-            if(display.search("\\-") !=-1)//check voor aftrekken
-            {
-                var parts = display.split("-");
-                antwoord = parseFloat(parts[0])-parseFloat(parts[1]);
-            }
-            if(display.search("\\*") !=-1)//check voor vermenigvuldigen
-            {
-                var parts = display.split("*");
-                antwoord = parseFloat(parts[0])*parseFloat(parts[1]);
-            }
+            var antwoord = eval(display);
             document.getElementById('display').value = antwoord;
         }
         else
