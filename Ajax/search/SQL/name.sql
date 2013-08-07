208355bceabb250d82dfa6e7b9ba4c7ab599a73c
bakeds <bakeds@gmail.com>
2013-08-07 12:55:38 +0200

    sql script voorbeeld database toegevoegd

---
+++ Ajax/search/SQL/name.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `name` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
