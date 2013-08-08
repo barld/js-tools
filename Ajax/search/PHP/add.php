c11053986b2a01505d01e7535a6c88aa09db0dc5
bakeds <bakeds@gmail.com>
2013-08-08 12:07:54 +0200

    google analitics toegevoegd

--- Ajax/search/PHP/add.php
+++ Ajax/search/PHP/add.php
@@ -1,3 +1,17 @@
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-37303062-1']);
+    _gaq.push(['_setDomainName', 'barld.nl']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
 <a href="../">back</a>
 <form method="POST">
     <label for="naam">naam</label>
