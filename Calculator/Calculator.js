2257369afea5abcfacf2fc25717dcb95e4f3f6b4
bakeds <bakeds@gmail.com>
2013-08-10 11:17:34 +0200

    over gegaan naar jquery en bug opgelost
    je kan nu midden in een string blijfen typen met het toetsenbord
    
    ook is het omgezet naar jquery voor zover ik nu even weet dit is voor
    somige dingen handiger en soms nog efiecenter ook

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -3,10 +3,25 @@
  * Date: 10-7-13
  * Time: 14:15
  */
-
+String.prototype.placeAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index);
+}
 
 function Calculator()
 {
+
+    /*
+        *   instaleer en zet de html objecten die gebruikt worden
+        *
+        *   @valeu : this.display_sum
+        *   @description : invoer veld waar de user de formule invoerd
+        *
+        *   @valeu : this.display_answer
+        *   @description : in dit gedisabled invoer veld word het antwoord getoond
+    */
+
+    this.display_sum = $('#display_sum');
+    this.disolay_answer = $('#display_answer');
 
    this.Answeregiven = false
 
@@ -14,10 +29,10 @@ function Calculator()
     {
         if(field === "sum")
         {
-            return document.getElementById('display_sum').value;
+            return this.display_sum.val();
         }else if(field === "answer")
         {
-            return document.getElementById('display_answer').value;
+            return this.disolay_answer.val();
         }
         return false
     };
@@ -26,14 +41,12 @@ function Calculator()
     {
         if(field === "sum")
         {
-            document.getElementById('display_sum').value = value;
-            return true;
+            this.display_sum.val(value);
         }else if(field === "answer")
         {
-            document.getElementById('display_answer').value = value;
-            return true;
+            this.disolay_answer.val(value);
         }
-
+        return true;
     };
 
     // functie om in display getallen en symbolen toe te voegen.
@@ -47,7 +60,13 @@ function Calculator()
         if(this.getDisplay("sum").length<20)
         {
             var firstnumber = this.getDisplay("sum");
-            this.setDisplay(firstnumber+number, "sum");
+            //kijk waar de cursor is om het teken op de juiste plaats te plaatsen
+            var placeCursor = this.display_sum.prop("selectionStart");
+            this.setDisplay(firstnumber.placeAt(placeCursor, number), "sum");
+            //hier een een dubele regel waar de selectie moet beginnen en eindigen deze is gelijk zodat er niks wordt geselecteerd
+            // +1 zodat de cursor er wel achter staat
+            this.display_sum.prop("selectionStart", placeCursor+1);
+            this.display_sum.prop("selectionEnd", placeCursor+1);
             this.Answeregiven = false;
         }
     };
@@ -77,7 +96,7 @@ function Calculator()
         }
         else
         {
-            var value = this.getDisplay();
+            var value = this.getDisplay('sum');
             this.setDisplay(value.substr(0, value.length-1), "sum");
         }
     };
